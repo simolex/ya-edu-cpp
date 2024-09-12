7652baaa08342f8b67d02850199df14bc418ad65
simolex <simolex@yandex.ru>
2024-09-12 16:55:39 +0300

    save results

---
+++ chapter_2_1/task_2_A.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that"<< std::endl;
+  std::cout << "  - is a better C"<< std::endl;
+  std::cout << "  - supports data abstraction"<< std::endl;
+  std::cout << "  - supports object-oriented programming"<< std::endl;
+  std::cout << "  - supports generic programming."<< std::endl;
+
+}
---
+++ chapter_2_2/task_2_2_B.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdint>
+
+int main() {
+    uint64_t   n;
+    std::cin >> n;
+    std::cout <<  n * (n + 1) / 2 << "\n";
+}
